8f61f6065ac22c8ac35c85a04305735ed92d922b
rishi314 <77830230+rishi314@users.noreply.github.com>
2022-03-02 11:25:45 +0530

    Updated Demo.java file for testing fetch command

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@ class Demo
 	public static void main(String args[])
 	{
 		System.out.println("Updating text for testing rebase from github repoitory");
+		System.out.println("Testing purpose for fetch command");
 		//System.out.println("Testing rebase in git, adding this text from featureBranch");
 	}
 }
